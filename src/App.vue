9c82fbeca79c0b077e75d7aa48822f5634d464a4
Lucas <lgomes@post.com>
2023-03-30 15:17:52 -0300

    action of login button

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,57 @@
+
 <template>
   <div id="app">
-    <div class="container">
+  <!-- <div class="container">
       <PrincipalLayout />
-    </div>
-    <!-- <div class="container">
+                  </div> -->
+  <!-- <div class="container">
       <TokenLogin />
-    </div> -->
+                  </div> -->
+    <div class="container">
+      <component v-bind:is="component" />
+    </div>
   </div>
 </template>
 
 <script>
 
-// import TokenLogin from './components/TokenLogin/TokenLogin.vue'
+import TokenLogin from './components/TokenLogin/TokenLogin.vue'
 import PrincipalLayout from './components/PrincipalLayout/PrincipalLayout.vue'
+import { ref, onMounted } from 'vue'
 
 export default {
   name: 'App',
 
   components: {
-    // TokenLogin,
+    TokenLogin,
     PrincipalLayout
   },
+  setup() {
+    let isLogged = false //function
+    let layout = ref('TokenLogin')
 
-  data() {
+    if (isLogged)
+      layout = ref('PrincipalLayout')
+
+    onMounted(() => {
+      window.electron.receive("loginAuth", (data) => {
+        if (data) {
+          console.log(`Received from auth login`)
+          layout.value = 'PrincipalLayout'
+
+        }
+      })
+    })
+
     return {
       dialog: false,
       notifications: false,
       sound: true,
       widgets: false,
+      component: layout,
     }
-  },
+
+  }
 }
 </script>
 
