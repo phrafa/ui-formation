928372704b73c690f800897354cf3fadf9c547e3
wilson Alves <wilson.alves@sumup.com>
2023-03-30 13:54:45 -0300

    listagem de email autenticado

--- src/backend/service/octokitService.js
+++ src/backend/service/octokitService.js
@@ -1,4 +1,4 @@
-const { Octokit, App } = require("octokit");
+const { Octokit } = require("octokit");
 
 
 class OctokitService {
@@ -12,14 +12,36 @@ class OctokitService {
     }
 
     async getAutenticatedEmailList() {
-        const data = await octokit.request('GET /user/emails', {
+        const data = await this.octokit.request('GET /user/emails', {
             headers: {
               'X-GitHub-Api-Version': '2022-11-28'
             }
-          })
-          return data
+          });
+          return data['data']
     }
+
+    async getSumupEmail() {
+        const emails = await this.getAutenticatedEmailList()
+        const sumupEmail = emails.filter((email)=>{
+            if(!email['email'].includes("sumup.com")) {
+                return false;
+            }
+            return true;
+        });
+        if(sumupEmail.length > 0) {
+            return sumupEmail[0]['email']
+        }
+        return null
+    }
+
+    
+
 
 }
 
 module.exports = OctokitService;
+
+(async () => {
+    os = new OctokitService('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')
+    console.log(await os.getSumupEmail())
+})();
