4d30b45907629afbc9c2a00dd766fc8e13f5a170
wilson Alves <wilson.alves@sumup.com>
2023-03-30 22:19:17 -0300

    remove test

--- src/backend/service/userService.js
+++ src/backend/service/userService.js
@@ -46,15 +46,3 @@ class UserService {
 }
 
 module.exports = UserService;
-
-(async() =>{
-    const us = new UserService()
-    us.loadProjects().then(projects => {
-        projects.map(async project => {
-            const contents = await us.loadProjectContents(project, project.environments[0])
-
-            console.log(project)
-            console.log(contents)
-        })
-    })
-})()
