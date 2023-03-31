d7f1abd352e80be7ef6e5b71da75a6f1bca31158
Rafael Reis <rafael.reis@sumup.com>
2023-03-30 23:48:30 -0300

    ajustes

--- src/backend/controllers/getProjectsControler.js
+++ src/backend/controllers/getProjectsControler.js
@@ -7,10 +7,22 @@ export async function execute(event, args) {
         const user = new UserService();
 
         const projects = await user.loadProjects()
-
         
+        const namespaces = projects.filter((item, index) => {
+            return projects.findIndex(obj => obj.namespace === item.namespace) === index;
+        }).map(item => {
+            return { namespace: item.namespace, teamName: item.teamName, projects: {} };
+          });
 
-        event.reply('getProjectsControler', projects)
+        for (let x in namespaces) {
+            namespaces[x].projects = projects.filter((item, index) => {
+               return namespaces[x].namespace === item.namespace
+            }).map(item => {
+                return { name: item.name};
+            });
+        }
+
+        event.reply('getProjectsControler', namespaces)
     } catch (error) {
         console.log({ error })
     }
--- src/components/RepositoryCards/RepositoryCards.vue
+++ src/components/RepositoryCards/RepositoryCards.vue
@@ -1,40 +1,43 @@
 <template>
-    <v-container>
-        <v-col v-for="(item) in items" :key="item.id">
-            <v-card style="background-color: #333; color:#FFF">
-                <v-card-title style="font-size: 16px; font-weight: bold;">
-                    {{ item.name }}
-                </v-card-title>
-                <v-card-text style="font-style: italic;">
-                    {{ item.namespace }}
-                </v-card-text>
-            </v-card>
-        </v-col>
-    </v-container>
+  <v-container>
+    <v-col v-for="item in items" :key="item.id">
+      <v-card style="background-color: #333; color: #fff">
+        <v-card-title style="font-size: 16px; font-weight: bold">
+          {{ item.teamName }}
+        </v-card-title>
+
+        <v-card-text
+          v-for="project in item.projects"
+          :key="project.id"
+          style="font-style: italic"
+        >
+          {{ project.name }}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-container>
 </template>
   
 <script>
-
-import { ref, onMounted } from 'vue'
-
+import { ref, onMounted } from "vue";
 
 export default {
-    setup() {
-        let projects = ref()
-        onMounted(async () => {
-            setTimeout(() => {
-                window.electron.send("getProjectsControler");
-            }, 100)
+  setup() {
+    let projects = ref();
+    onMounted(async () => {
+      setTimeout(() => {
+        window.electron.send("getProjectsControler");
+      }, 100);
 
-            await window.electron.receive("getProjectsControler", (data) => {
-                if (data) {
-                    projects.value = data
-                }
-            })
-        })
+      await window.electron.receive("getProjectsControler", (data) => {
+        if (data) {
+          projects.value = data;
+        }
+      });
+    });
 
-        return { items: projects }
-    }
-}
+    return { items: projects };
+  },
+};
 </script>
   
